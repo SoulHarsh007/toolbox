ce6b0f84a90ba7d5c30a0884b8ae4b37e766a083
MeDrioX <medriox@gmail.com>
2020-05-08 23:48:22 +0200

    Update.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 module.exports = class ToolBox {
 
     constructor() {
-        this.loggerr = new (require('./modules/logger/Logger'));
+        this.logger = new (require('./modules/logger/Logger'));
         this.random = new (require('./modules/random/Random'));
     }
 
